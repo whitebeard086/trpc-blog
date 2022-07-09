ff9ab7524176e88a5dfc89a75c292a0c0d631e09
The Exorcist <whitebeard086@gmail.com>
2022-07-09 21:12:43 +0100

    configure app router

---
+++ src/server/createRouter.ts
@@ -0,0 +1,7 @@
+import superjson from "superjson";
+import { router } from "@trpc/server";
+import { Context } from "./createContext";
+
+export function createRouter() {
+  return router<Context>().transformer(superjson);
+}
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,4 +1,4 @@
 import { createReactQueryHooks } from "@trpc/react";
+import { AppRouter } from "../server/route/app.router";
 
-// todo: add appRouter as generic
-export const trpc = createReactQueryHooks();
+export const trpc = createReactQueryHooks<AppRouter>();
